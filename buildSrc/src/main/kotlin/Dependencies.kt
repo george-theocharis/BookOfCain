d85992d33d3e9982d1b2b75de98e227badcd34ee
giorgosTh <giwrgos.theocharis@gmail.com>
2020-03-08 19:51:05 +0200

    [BOC-0002] build: Move android ktx to kts

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,6 +3,7 @@ object Versions {
     const val kotlin = "1.3.61"
     const val constraintLayout = "1.1.3"
     const val appCompat = "1.1.0"
+    const val androidKtx = "1.2.0"
 }
 
 object Libs {
@@ -10,5 +11,6 @@ object Libs {
     const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
     const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    const val androidKtx = "androidx.core:core-ktx:${Versions.androidKtx}"
 
 }
