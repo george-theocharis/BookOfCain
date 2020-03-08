2e92ff53911f65b1584cb0b90dc55bfa9ec3c3a0
giorgosTh <giwrgos.theocharis@gmail.com>
2020-03-08 19:49:13 +0200

    [BOC-0002] build: Move kotlin std lib to kts

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,11 +1,13 @@
 object Versions {
 
+    const val kotlin = "1.3.61"
     const val constraintLayout = "1.1.3"
     const val appCompat = "1.1.0"
 }
 
 object Libs {
 
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
     const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
 
