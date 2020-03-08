66210d9aac26e6758b9f435f8d75990eb0121d07
giorgosTh <giwrgos.theocharis@gmail.com>
2020-03-08 19:44:40 +0200

    [BOC-0002] build: Move constraint layout to kts

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,11 @@
+object Versions {
+
+    const val constraintLayout = "1.1.3"
+
+}
+
+object Libs {
+
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+
+}
