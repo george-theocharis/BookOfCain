e57f9fdf832f5894ec8898d5edcf54864675d8a8
giorgosTh <giwrgos.theocharis@gmail.com>
2020-03-08 19:46:05 +0200

    [BOC-0002] build: Move app compat to kts

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,11 +1,12 @@
 object Versions {
 
     const val constraintLayout = "1.1.3"
-
+    const val appCompat = "1.1.0"
 }
 
 object Libs {
 
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
 
 }
