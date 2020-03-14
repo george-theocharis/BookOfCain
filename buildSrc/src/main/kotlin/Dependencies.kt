a988ab197cdbb3cf2386a930693f1e9d6cef1d1b
giorgosTh <giwrgos.theocharis@gmail.com>
2020-03-14 18:59:56 +0200

    [BOC-0003] feature: Add retrofit as a dependency
    In this commit a package is added to dependencies.kt in order to be able
    to access nested objects in build.gradle
    
    Also added compileOptions needed for retrofit

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,9 +1,17 @@
+package dependencies
+
 object Versions {
 
     const val kotlin = "1.3.61"
     const val constraintLayout = "1.1.3"
     const val appCompat = "1.1.0"
     const val androidKtx = "1.2.0"
+
+    object Network {
+
+        const val retrofit = "2.7.2"
+
+    }
 }
 
 object Libs {
@@ -12,5 +20,11 @@ object Libs {
     const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
     const val androidKtx = "androidx.core:core-ktx:${Versions.androidKtx}"
+
+    object Network {
+
+        const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.Network.retrofit}"
+
+    }
 
 }
