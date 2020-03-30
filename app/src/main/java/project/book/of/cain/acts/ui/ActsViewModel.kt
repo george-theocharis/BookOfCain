ff605e0c5001fe0b86daf2491f339b4fb1dd27cc
giorgosTh <giwrgos.theocharis@gmail.com>
2020-03-30 20:00:18 +0300

    [BOC-0006] test: Resolve testing view model scope

--- app/src/main/java/project/book/of/cain/acts/ui/ActsViewModel.kt
+++ app/src/main/java/project/book/of/cain/acts/ui/ActsViewModel.kt
@@ -3,8 +3,10 @@ package project.book.of.cain.acts.ui
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
-class ActsViewModel: ViewModel() {
+class ActsViewModel : ViewModel() {
 
     private val _state = MutableLiveData<ActsScreen.State>()
     fun state(): LiveData<ActsScreen.State> = _state
@@ -13,14 +15,18 @@ class ActsViewModel: ViewModel() {
         _state.value = ActsScreen.State()
     }
 
-    fun onEvent(event: ActsScreen.Event){
+    fun onEvent(event: ActsScreen.Event) {
         when (event) {
             is ActsScreen.Event.FetchActs -> onFetchActs()
         }
     }
 
     private fun onFetchActs() {
-        _state.value = _state.value?.copy(loading = true)
+        viewModelScope.launch {
+            _state.value = _state.value?.copy(loading = true)
+        }
     }
 
 }
+
+
--- app/src/test/java/project/book/of/cain/acts/ui/ActsViewModelTest.kt
+++ app/src/test/java/project/book/of/cain/acts/ui/ActsViewModelTest.kt
@@ -1,32 +1,57 @@
 package project.book.of.cain.acts.ui
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import org.junit.Assert.assertEquals
+import androidx.lifecycle.Observer
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verifyOrder
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
 
+@ExperimentalCoroutinesApi
 class ActsViewModelTest {
 
     @get:Rule
     val rule: TestRule = InstantTaskExecutorRule()
 
+    private val observerMock = mockk<Observer<ActsScreen.State>>()
     private lateinit var testedClass: ActsViewModel
+
+    private val testDispatcher = TestCoroutineDispatcher()
 
     @Before
     fun setup() {
+        Dispatchers.setMain(testDispatcher)
+
         testedClass = ActsViewModel()
+
+        every { observerMock.onChanged(any()) } returns Unit
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
     }
 
     @Test
-    fun `given an initial state, when fetching acts, then state is loading`() {
-        val expected = ActsScreen.State(loading = true)
+    fun `given an initial state, when fetching acts, then state is loading`() = runBlockingTest {
+        val loading = ActsScreen.State(loading = true)
+        testedClass.state().observeForever(observerMock)
 
         testedClass.onEvent(ActsScreen.Event.FetchActs)
-        val actual = testedClass.state().value
 
-        assertEquals(expected, actual)
+        verifyOrder {
+            observerMock.onChanged(loading)
+        }
     }
 
 }
