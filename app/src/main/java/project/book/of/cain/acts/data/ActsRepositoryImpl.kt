7f63d8b164a139b07c6f1ce6de1a485464333a8f
giannispapageorgiou <g.papageorgiou@thebeat.co>
2020-04-04 17:06:36 +0300

    [BOC-0006] feature: Add acts repository implementation

---
+++ app/src/main/java/project/book/of/cain/acts/data/ActsRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package project.book.of.cain.acts.data
+
+import project.book.of.cain.acts.domain.Act
+import project.book.of.cain.acts.domain.ActsRepository
+import project.book.of.cain.utils.Either
+import project.book.of.cain.utils.Error
+
+class ActsRepositoryImpl(
+    private val actsApi: ActsApi
+): ActsRepository {
+
+    override suspend fun getActs(): Either<Error, List<Act>> {
+        return try {
+            val actsList = actsApi.getActs()
+            Either.Success(actsList.map { it.toAct() })
+        } catch (ex: Exception) {
+            Either.Failure(ex.toError())
+        }
+    }
+
+    private fun Exception.toError(): Error = message?.let{ Error.SimpleError(it) } ?: Error.SimpleError()
+
+}
