c4cd23bc9d033593e7ba4fbfa1afc6b986882ef5
giorgosTh <giwrgos.theocharis@gmail.com>
2020-04-05 15:32:56 +0300

    [BOC-0006] refactor: Use view binding to main activity to show state

--- app/src/main/java/project/book/of/cain/MainActivity.kt
+++ app/src/main/java/project/book/of/cain/MainActivity.kt
@@ -1,27 +1,28 @@
 package project.book.of.cain
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import project.book.of.cain.acts.ui.ActsScreen
 import project.book.of.cain.acts.ui.ActsViewModel
+import project.book.of.cain.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private val actsViewModel by viewModel<ActsViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        actsViewModel.state().observe(this, Observer<ActsScreen.State> { render(it) })
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        actsViewModel.state().observe(this, Observer { render(it) })
 
         actsViewModel.onEvent(ActsScreen.Event.FetchActs)
     }
 
     private fun render(state: ActsScreen.State) {
-        Log.d("STATE = ", state.toString())
+        binding.text.text = state.toString()
     }
 }
